16c25aba9d1035b9d4b51f78fa4a46d46ff5e671
user <none>
2020-11-16 02:58:25 +0500

    4.2. Тернарное сравнение [#282328]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,15 +8,23 @@ public class MaxTest {
     @Test
     public void max2is() {
         assertEquals(4, Max.max(3, 4));
+        assertEquals(4, Max.max(4, 3));
+        assertEquals(4, Max.max(4, -3));
+        assertEquals(-4, Max.max(-4, -5));
+
+
     }
 
     @Test
     public void max2not() {
         assertEquals(3, Max.max(3, 3));
+        assertEquals(0, Max.max(0, 0));
+        assertEquals(-1, Max.max(-1, -1));
+
     }
     @Test
     public void max3() {
-        assertEquals(3, Max.max(1,2,3));
+        assertEquals(3, Max.max(3,2,1));
     }
 
     @Test
