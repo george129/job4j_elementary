853b377f6c7ac7316db98e427f6d7cd453fadac0
user <none>
2020-11-17 00:05:52 +0500

    6.0.5. Массивы в тестах. [#282350]

---
+++ test/ru/job4j/loop/SquareTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SquareTest {
+    @Test
+    public void whenBound3Then014() {
+        int bound = 3;
+        int[] rst = Square.calculate(bound);
+        int[] expect = new int[] {0, 1, 4};
+        assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void bounds5() {
+        assertThat(Square.calculate(5), is(new int[]{0, 1, 4, 9, 16}));
+    }
+}
